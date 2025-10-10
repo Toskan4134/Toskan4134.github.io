87738c8d1025fabfe11f3fc2cbb02bfdf0ec39ad
Toskan4134 <angama4134@gmail.com>
2025-10-10 18:52:28 +0200

    Fixed Project image

--- src/lib/lists.js
+++ src/lib/lists.js
@@ -220,7 +220,7 @@ export const projects = [
             name: 'Descargar',
             url: 'https://github.com/Toskan4134/PokeShop/releases/latest',
         },
-        image: 'https://private-user-images.githubusercontent.com/62228706/492068677-4d4650fc-9f38-4b2d-b1e5-8e31c6a59c10.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NjAxMTQ1ODAsIm5iZiI6MTc2MDExNDI4MCwicGF0aCI6Ii82MjIyODcwNi80OTIwNjg2NzctNGQ0NjUwZmMtOWYzOC00YjJkLWIxZTUtOGUzMWM2YTU5YzEwLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTEwMTAlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUxMDEwVDE2MzgwMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTk2YTRmODZlYTEyNGFiN2E2Y2ZjMzE4ODIxYzEwMmEyMTUxMDg1M2M0Y2JmNmRlNTIxYzMwYjZkNTFmOWE0NTkmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.bYWJxPGV9tdwC2_KPzLP57_M-UrMaFSiQ1V8d6zb2h4',
+        image: 'https://i.imgur.com/HvxW6Ov.png',
     },
     {
         title: 'LethalRegeneration',
